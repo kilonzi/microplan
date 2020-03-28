08a40382ba99e714b8d5b53ff8c90a48bda4661a
kilonzi <mrjohnkilonzi@gmail.com>
2020-03-28 09:13:01 +0300

    Fix: Added google maps support instead of HereMaps

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -1,50 +1,86 @@
 <template>
   <section>
-    <div id="mapContainer" class="map-container">
-        <div v-if="latitude =='' & longitude == ''">
-            <button @click="getLocation" class="primary">Reload Map</button>
-        </div>
+    <div v-if="latitude =='' & longitude == ''">
+      <button @click="getLocation" class="primary">Reload Map</button>
     </div>
-    <div id="toolsContainer"></div>
+    <div class="section-content">
+      <div id="mapContainer" class="map-container"></div>
+      <div id="toolsContainer" class="tools-container">
+        <div class="toggle"></div>
+        <div class="tools">
+          <h5>Tools & Markers</h5>
+          <div class="areas">
+            <p>
+              <b>Area</b>
+            </p>
+            <div class="shapes">
+              <i class="circular large hand point up teal icon"></i>
+              <div class="circle"></div>
+              <div class="rectangle"></div>
+            </div>
+          </div>
+          <div class="points">
+            <p>
+              <b>Markers</b>
+            </p>
+            <div v-for="(point,index) in pointers" :key="index">
+              <div :data-wenk="point.key">
+                <i :class="point.location"></i>
+                <i>{{point.location}}</i>
+              </div>
+            </div>
+          </div>
+          <div class="actions">
+            <button class="primary">Save</button>
+            <button>Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+var geolocation = require("geolocation");
 export default {
   data() {
     return {
-      latitude: '',
-      longitude: ''
+      latitude: 0,
+      longitude: 0,
+      language: "en-US",
+      pointers: {
+        "location": "plus icon",
+        "hospital":"H square icon"
+      }
     };
   },
   mounted() {
     this.getLocation();
+    // this.initMap()
   },
   methods: {
-    initializeMap() {
-      var platform = new window.H.service.Platform({
-        apikey: "v2cq2txgLz35lSTPicOEJbKO6o-cK1fO4e7HcKp5MHo"
-      });
-      var defaultLayers = platform.createDefaultLayers();
-      new window.H.Map(
-        document.getElementById("mapContainer"),
-
-        defaultLayers.vector.normal.map,
-        {
-          zoom: 10,
-          center: { lat: this.latitude, lng: this.longitude }
-        }
-      );
-    },
+      initMap(){
+          new window.google.maps.Map(document.getElementById('mapContainer'), {
+          center: {lat: this.latitude, lng: this.longitude},
+          zoom: 14
+        });
+      },
     getLocation() {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(this.showPosition);
+      } else {
+        geolocation.getCurrentPosition(function(err, position) {
+          if (err) throw err;
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+          this.initializeMap();
+        });
       }
     },
     showPosition(position) {
       this.latitude = position.coords.latitude;
       this.longitude = position.coords.longitude;
-      this.initializeMap();
+      this.initMap();
     }
   }
 };
@@ -57,7 +93,7 @@ section {
   margin: 1px;
 }
 .map-container {
-  height: 100%;
+  height: 100vh;
   width: 100%;
 }
 button {
@@ -74,5 +110,48 @@ button {
 }
 .secondary {
   border-color: black;
+}
+.section-content {
+  display: flex;
+}
+.tools-container {
+  width: 20%;
+  max-width: 250px;
+  height: 100vh;
+  border-left: 1px solid teal;
+}
+.areas {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 80px;
+}
+.shapes {
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  padding: 5px;
+}
+
+.circle {
+  height: 50px;
+  width: 50px;
+  border: 1px solid teal;
+  border-radius: 50%;
+}
+.rectangle {
+  height: 50px;
+  width: 70px;
+  border: 1px solid teal;
+}
+.tools {
+  width: 100%;
+}
+.actions {
+  position: absolute;
+  bottom: 10px;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'sanitize.css'
+import 'semantic-ui-icon/icon.css'
+import 'wenk/dist/wenk.css'
 
 Vue.config.productionTip = false
 
